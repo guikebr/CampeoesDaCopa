d7b917e616c77ca2fa21d44d809d7a6c914c5613
guilherme <guilherme.santos@clubecandeias.com>
2019-11-20 13:08:10 -0300

    CampeoesDaCopa
    Aplicativo que mostra as rodadas da copa.

---
+++ CampeoesDaCopa/Models/WorldCup.swift
@@ -0,0 +1,31 @@
+//
+//  WorldcuP.swift
+//  CampeoesDaCopa
+//
+//  Created by Guilherme Santos on 15/05/19.
+//  Copyright © 2019 Guilherme Santos. All rights reserved.
+//
+
+import Foundation
+
+struct WorldCup: Codable {
+    let year: Int
+    let country: String
+    let winner: String
+    let vice: String
+    let winnerScore: String
+    let viceScore: String
+    let matches: [Match]
+}
+
+struct Match: Codable {
+    let stage: String
+    let games: [Game]
+}
+
+struct Game: Codable {
+    let home: String
+    let away: String
+    let score: String
+    let date: String
+}
